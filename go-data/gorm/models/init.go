8a2f4622b6a5537129c8f8c303a7602f2cba7a7f
xingcuntian <xingcuntian@qq.com>
2016-12-30 18:01:32 +0800

    update

--- go-data/gorm/models/init.go
+++ go-data/gorm/models/init.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+    "github.com/jinzhu/gorm"
+    _ "github.com/go-sql-driver/mysql"
+)
+
+var db *DB
+
+func init(){
+    var err error
+    db,err = gorm.Open("mysql","xingcuntian:xingcuntian@2016@/gotest?charset=utf8&parseTime=True&loc=Local")
+    defer db.Close()
+}
